23a9fc8b117a847f7af6a20ce390c99e89525203
dcmax <mdeco@2dc.it>
2019-01-21 16:32:11 +0100

    Update standard02.ps1

--- standard02.ps1
+++ standard02.ps1
@@ -1,7 +1,7 @@
 # configurazione standard per PC
 
 # Boxstarter options
-$Boxstarter.RebootOk=$false # Allow reboots?
+$Boxstarter.RebootOk=$true # Allow reboots?
 $Boxstarter.NoPassword=$false # Is this a machine with no login password?
 $Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
 $env:ChocolateyAllowEmptyChecksums=$true
